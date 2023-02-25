4465f85f3bb2ece4663d89c50c7eb15fb361ce65
Jeroen Brinkhorst <jeroenb04@live.com>
2023-02-26 00:13:47 +0100

    feat: Commands

--- week_3/app/src/geometric/CommandUI.java
+++ week_3/app/src/geometric/CommandUI.java
@@ -9,22 +9,50 @@ public class CommandUI {
         String w = getUserInput().toLowerCase();
 
         while(!w.equals("quit")) {
-            String[] parts = w.split(" ");
+            String[] args = w.split(" ");
 
-            switch(parts[0]) {
+            switch(args[0]) {
                 case "show":
+                    Geometric[] shapes = shapeList.getShapes();
+
+                    for (Geometric shape : shapes) {
+                        System.out.println(shape.toString());
+                    }
                     break;
+
                 case "circle":
+                    double cx = Double.parseDouble(args[1]);
+                    double cy = Double.parseDouble(args[2]);
+                    double cr = Double.parseDouble(args[3]);
+                    shapeList.addShape(new Circle(cx, cy, cr));
                     break;
+
                 case "rectangle":
+                    double rx = Double.parseDouble(args[1]);
+                    double ry = Double.parseDouble(args[2]);
+                    double rw = Double.parseDouble(args[3]);
+                    double rh = Double.parseDouble(args[4]);
+                    shapeList.addShape(new Rectangle(rx, ry, rw, rh));
                     break;
+
                 case "move":
+                    int mi = Integer.parseInt(args[1]);
+                    double mx = Double.parseDouble(args[2]);
+                    double my = Double.parseDouble(args[3]);
+
+                    Geometric shape = shapeList.find(mi);
+
+                    shape.move(mx, my);
                     break;
+
                 case "remove":
+                    int ri = Integer.parseInt(args[1]);
+
+                    shapeList.removeShapeByIndex(ri);
                     break;
+
                 case "sort":
-                    break;
-                case "help":
+                    System.out.print("temp\n");
                     break;
                 default:
                     System.out.println("Command not recognized. Please try again.\n");
