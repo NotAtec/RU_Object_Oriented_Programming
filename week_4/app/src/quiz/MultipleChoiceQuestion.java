66f431547c06757f0e1adf45508a4b0dc2716a62
Jeroen Brinkhorst <jeroenb04@live.com>
2023-03-05 15:13:05 +0100

    fix: Conform to spec

--- week_4/app/src/quiz/MultipleChoiceQuestion.java
+++ week_4/app/src/quiz/MultipleChoiceQuestion.java
@@ -3,7 +3,7 @@ package quiz;
 public class MultipleChoiceQuestion extends Question {
     private final String[] options;
     private final int correctOption;
-
+    private final String[] letters = {"a)", "b)", "c)", "d)", "e)", "f)", "g)", "h)", "i)", "j)"};
     public MultipleChoiceQuestion(String question, String[] answers, int correctAnswer, int score) {
         super(question, answers[correctAnswer], score);
         options = answers;
@@ -19,12 +19,25 @@ public class MultipleChoiceQuestion extends Question {
     @Override
     public String toString() {
         StringBuilder builtString = new StringBuilder();
-        String[] letters = {"a)", "b)", "c)", "d)", "e)", "f)", "g)", "h)", "i)", "j)"};
 
         for (int i = 0; i < options.length; i++) {
             builtString.append(letters[i]).append(" ").append(options[i]).append("\n");
         }
 
         return super.toString() + builtString;
+    }
+
+    @Override
+    public String correctAnswer() {
+        return "The correct answer is: (" + letters[correctOption] + ". Which corresponds to: " + options[correctOption] + "\n";
+    }
+
+    @Override
+    public boolean isCorrect(String guess) {
+        String modified = guess + ")";
+        for (int i = 0; i < letters.length; i++) {
+            if(letters[i].equalsIgnoreCase(modified)) return correctOption == i;
+        }
+        return false;
     }
 }
--- week_4/app/src/quiz/OpenQuestion.java
+++ week_4/app/src/quiz/OpenQuestion.java
@@ -9,4 +9,19 @@ public class OpenQuestion extends Question {
     public OpenQuestion(String question, String answer) {
         super(question, answer, 3);
     }
+
+    @Override
+    public String correctAnswer() {
+        return super.correctAnswer();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean isCorrect(String ans) {
+        return super.isCorrect(ans);
+    }
 }
