4bf8ebb21a893e1ad227e1e621e0af7320ca37da
Jeroen Brinkhorst <jeroen.brinkhorst@ru.nl>
2023-03-12 13:50:57 +0100

    Week 5 OOP Assignment (Solved) (#3)
    * chore: Add wk4 files
    
    * feat: Abstract Question Class
    
    * feat: Open Question Type
    
    * feat: MQ Question Type
    
    * fix: Conform to spec
    
    * fix: Super behaviour with classes
    
    * feat: Add this/that question
    
    * feat: Game UI / Round 1 processing
    
    * chore: Fix gradle builds
    
    * fix: Correct display of this/that question.
    
    * feat: 2 game-rounds are played.
    
    * fix: correct display of wrong answers (OQ)
    
    * feat: Week 5, add prev encountered to solver
    
    * equal method Solver
    
    * fix: find() for encountered.
    
    * implementation isSolution
    
    * feat: Solver uses Full path from root as result string.
    
    * ik hoop dat successors klaar is
    
    * manhattendistance klaar, successors en compareTo
    
    * updated manhattendistance
    
    * implemented getPerent()
    
    * row col,  x en y gefixet
    
    * verbeterde versie, ik heb ideeen
    
    * ander probleem: er komen steeds meer holes
    
    * de solver werkt, alleen de path laten zien niet
    
    * fix: Import all utils to Solver
    
    * fix: add `chmod +x` to gradlew
    
    * fix: Make sure String creation for full solution works
    
    Done by assigning the parent of each sucessor.
    
    * fix: Unfucked it again. 3x3 works now.
    
    * feat: Implement HashMap instead of List for encountered
    
    * chore: Remove all unnecessary imports
    
    ---------
    
    Co-authored-by: Floris <reuvers.floris@gmail.com>

---
+++ week_5/app/src/slidingGame/Configuration.java
@@ -0,0 +1,60 @@
+package slidingGame;
+/**
+ * An interface for representing nodes in a state space.
+ */
+
+import java.util.Collection;
+import java.util.List;
+import java.util.LinkedList;
+
+public interface Configuration extends Comparable<Configuration> {
+	/**
+	 * To obtain the parent of the current configuration, i.e. the configuration
+	 * which had this as one of its successors
+	 *
+	 * @return a reference to the parent
+	 */
+	public abstract Configuration getParent();
+
+	/**
+	 * To obtain the successors for a specific configuration
+	 *
+	 * @return a collection of configurations containing the successors
+	 */
+	public abstract Collection<Configuration> successors();
+
+	/**
+	 * For marking final / solution configurations.
+	 *
+	 * @return true if a this is a solution, false otherwise
+	 */
+	public abstract boolean isSolution();
+
+	/**
+	 * To build a path from the root configuration to the current one.
+	 *
+	 * @return a list of successive configurations from the root to 'this'
+	 */
+	public default List<Configuration> pathFromRoot() {
+		List<Configuration> path = new LinkedList<Configuration>();
+		path.add(this);
+		Configuration step = this;
+
+		while(step.getParent() != null) {
+			path.add(step.getParent());
+			step = step.getParent();
+		}
+
+		return this.reverseList(path);
+	}
+
+	private List<Configuration> reverseList(List<Configuration> list) {
+		List<Configuration> reversed = new LinkedList<Configuration>();
+
+		for (int i = list.size() - 1; i >= 0; i--) {
+			reversed.add(list.get(i));
+		}
+
+		return reversed;
+	}
+}
