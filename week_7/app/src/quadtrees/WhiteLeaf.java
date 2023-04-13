fe08162a46f528b8a0e9d14317546490a62100b4
Floris <reuvers.floris@gmail.com>
2023-04-13 16:27:41 +0200

    Implement WhiteLeaf and BlackLeaf

--- week_7/app/src/quadtrees/WhiteLeaf.java
+++ week_7/app/src/quadtrees/WhiteLeaf.java
@@ -3,13 +3,13 @@ package quadtrees;
 import java.io.Writer;
 import java.io.IOException;
 
-public class BlackLeaf implements QuadTreeNode{
+public class WhiteLeaf implements QuadTreeNode{
 
     @Override
     public void fillBitmap(int x, int y, int width, Bitmap bitmap) {
         for (int x1 = x; x1 < x + width; x1++) {
             for (int y1 = y; y1 < y + width; y1++) {
-                bitmap.setBit(x1, y1, false);
+                bitmap.setBit(x1, y1, true);
             }
         }
     }
@@ -17,7 +17,7 @@ public class BlackLeaf implements QuadTreeNode{
     @Override
     public void writeNode(Writer out) {
         try {
-            out.append("00");
+            out.append("01");
         } catch(IOException e) {
             // TODO: handle exception
         }
